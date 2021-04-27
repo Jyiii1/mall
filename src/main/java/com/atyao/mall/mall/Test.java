7f0e5b39ca1a4bdc807846dda5738548892d84d3
jy <354705830@qq.com>
2021-04-27 10:51:13 +0800

    hotfix commit

--- src/main/java/com/atyao/mall/mall/Test.java
+++ src/main/java/com/atyao/mall/mall/Test.java
@@ -3,6 +3,6 @@ package com.atyao.mall.mall;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello,world");
-        System.out.println("123");
+
     }
 }
